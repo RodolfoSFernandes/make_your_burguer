a58d9ea6c78e5da6770a575685dd32e1773fd7da
Rodolfo Fernandes <rodolfo.fernandes@navarraaluminio.com>
2023-09-25 17:06:10 +0100

    atualização 25

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Crie o seu hamburguer</h1>
+    <Burgerform/>
+  </div>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue"
+import Burgerform from "../components/Burgerform.vue"
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+    Burgerform
+  }
+}
+</script>
