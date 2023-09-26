24e73c1b4c946dea195bdffeadfeb0377d1ae712
Rodolfo Fernandes <rodolfo.fernandes@navarraaluminio.com>
2023-09-26 16:40:16 +0100

    Atualização final

--- src/components/Burgerform.vue
+++ src/components/Burgerform.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <p>Component de Mensagem</p>
+        <Message :msg="msg" v-show="msg"/>
         <div>
-            <form id="hamburger-form">
+            <form id="hamburger-form" @submit="createBurger">
                 <div class="input-container">
                     <label for="nome">Nome do cliente:</label>
                     <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
@@ -37,6 +37,8 @@
 </template>
 
 <script>
+import Message from "./Message.vue";
+
 export default {
     name: "Burgerform",
     data() {
@@ -48,7 +50,6 @@ export default {
             pao: null,
             carne: null,
             opcionais: [],
-            status: "Solicitado",
             msg: null
         }
     },
@@ -62,11 +63,49 @@ export default {
            this.carnes = data.carnes;
            this.opcionaisdata = data.opcionais;
 
+        },
+        async createBurger(e) {
+            e.preventDefault();
+
+            const data = {
+                nome: this.nome,
+                carne:this.carne,
+                pao: this.pao,
+                opcionais: Array.from (this.opcionais),
+                status: "Solicitado"
+            }
+
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch("http://localhost:3000/burgers", {
+                method: "POST",
+                headers: { "content-Type": "application/json"},
+                body: dataJson
+            });
+
+            const res = await req.json();
+
+            //colocar uma msg de sistema
+            this.msg = `Pedido nº ${res.id} realizado com sucesso`;
+
+            //limpar msg
+            setTimeout(() => this.msg = "", 3000);
+
+            //limpar os campos
+            this.nome = "";
+            this.carne = "";
+            this.pao = "";
+            this.opcionais = "";
+            
         }
     },
     mounted() {
         this.getIngredientes()
+    },
+    components: {
+        Message
     }
+
 }
 </script>
 
